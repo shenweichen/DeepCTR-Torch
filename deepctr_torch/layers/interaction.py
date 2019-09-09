ccf234daa6f8ddbb99be95a445253c8c70960582
qidi xu <qidixu@yeah.net>
2019-09-09 12:42:07 +0800

    Add Bi-Interaction Pooling for NFM model.

--- deepctr_torch/layers/interaction.py
+++ deepctr_torch/layers/interaction.py
@@ -15,3 +15,30 @@ class FM(nn.Module):
         cross_term = 0.5 * torch.sum(cross_term, dim=2, keepdim=False)
 
         return cross_term
+
+
+class BiInteractionPooling(nn.Module):
+    """
+    Bi-Interaction Layer used in Neural FM, compress the 
+    pairwise element-wise product of features into one single vector.
+    
+    Input shape
+    - A 3D tensor with shape: ``(batch_size, 1, embedding_size)``.
+
+    Output shape
+    - 3D tensor with shape: ``(batch_size, 1, embedding_size)``.
+
+    References
+    - [He X, Chua T S. Neural factorization machines for sparse predictive analytics[C]//Proceedings of the 40th International ACM SIGIR conference on Research and Development in Information Retrieval. ACM, 2017: 355-364.](http://arxiv.org/abs/1708.05027)
+    """
+
+    def __init__(self):
+        super(BiInteractionPooling, self).__init__()
+
+    def forward(self, inputs):
+        concated_embeds_value = inputs
+        square_of_sum = torch.pow(torch.sum(concated_embeds_value, dim=1, keepdim=True), 2)
+        sum_of_square = torch.sum(concated_embeds_value * concated_embeds_value, dim=1, keepdim=True)
+        cross_term = 0.5 * (square_of_sum - sum_of_square)
+        return cross_term
+        
