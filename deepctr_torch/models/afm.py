c627b52cf1a8ec359c5c055c24a899ae52832593
shenweichen <weichen.swc@alibaba-inc.com>
2019-09-17 19:48:49 +0800

    update

--- deepctr_torch/models/afm.py
+++ deepctr_torch/models/afm.py
@@ -1,3 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+Author:
+    Weichen Shen,wcshen1994@163.com
+Reference:
+    [1] Xiao J, Ye H, He X, et al. Attentional factorization machines: Learning the weight of feature interactions via attention networks[J]. arXiv preprint arXiv:1708.04617, 2017.
+    (https://arxiv.org/abs/1708.04617)
+"""
 import torch
 import torch.nn.functional as F
 
--- deepctr_torch/models/wdl.py
+++ deepctr_torch/models/wdl.py
@@ -1,4 +1,10 @@
-from itertools import chain
+# -*- coding:utf-8 -*-
+"""
+Author:
+    Weichen Shen,wcshen1994@163.com
+Reference:
+    [1] Cheng H T, Koc L, Harmsen J, et al. Wide & deep learning for recommender systems[C]//Proceedings of the 1st Workshop on Deep Learning for Recommender Systems. ACM, 2016: 7-10.(https://arxiv.org/pdf/1606.07792.pdf)
+"""
 
 import torch.nn as nn
 import torch.nn.functional as F
