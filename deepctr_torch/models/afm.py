473542c13f2364b86ea2e6a13ef5dd51f769c01f
shenweichen <wcshen1994@163.com>
2019-09-22 14:39:52 +0800

    Update afm.py

--- deepctr_torch/models/afm.py
+++ deepctr_torch/models/afm.py
@@ -15,13 +15,8 @@ from ..layers import FM, AFMLayer
 
 class AFM(BaseModel):
 
-    def __init__(self,
-                 linear_feature_columns, dnn_feature_columns, embedding_size=8, use_attention=True, attention_factor=8,
-                 l2_reg_linear=1e-5, l2_reg_embedding=1e-5, l2_reg_att=1e-5, afm_dropout=0, init_std=0.0001, seed=1024,
-                 task='binary', device='cpu'):
-
         """Instantiates the Attentional Factorization Machine architecture.
-        
+
         :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
         :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
         :param embedding_size: positive integer,sparse feature embedding_size
@@ -36,6 +31,31 @@ class AFM(BaseModel):
         :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
         :param device
         :return: A PyTorch model instance.
+        
+        """
+
+    def __init__(self,
+                 linear_feature_columns, dnn_feature_columns, embedding_size=8, use_attention=True, attention_factor=8,
+                 l2_reg_linear=1e-5, l2_reg_embedding=1e-5, l2_reg_att=1e-5, afm_dropout=0, init_std=0.0001, seed=1024,
+                 task='binary', device='cpu'):
+
+        """Instantiates the Attentional Factorization Machine architecture.
+
+        :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
+        :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
+        :param embedding_size: positive integer,sparse feature embedding_size
+        :param use_attention: bool,whether use attention or not,if set to ``False``.it is the same as **standard Factorization Machine**
+        :param attention_factor: positive integer,units in attention net
+        :param l2_reg_linear: float. L2 regularizer strength applied to linear part
+        :param l2_reg_embedding: float. L2 regularizer strength applied to embedding vector
+        :param l2_reg_att: float. L2 regularizer strength applied to attention net
+        :param afm_dropout: float in [0,1), Fraction of the attention net output units to dropout.
+        :param init_std: float,to use as the initialize std of embedding vector
+        :param seed: integer ,to use as random seed.
+        :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+        :param device
+        :return: A PyTorch model instance.
+
         """
 
         super(AFM, self).__init__(linear_feature_columns, dnn_feature_columns, embedding_size=embedding_size,
