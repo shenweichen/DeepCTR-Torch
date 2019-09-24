f2835e9d0199eb153963f0b6cf7935de5cf367de
zengkai <277248199@qq.com>
2019-09-24 08:47:09 +0800

    make the code more robust

--- deepctr_torch/layers/sequence.py
+++ deepctr_torch/layers/sequence.py
@@ -20,8 +20,15 @@ class KMaxPooling(nn.Module):
         self.k = k
         self.axis = axis
         self.to(device)
-    
     def forward(self, input):
+        if self.axis < 0 or self.axis >= len(input.shape):
+            raise ValueError("axis must be 0~%d,now is %d" %
+                             (len(input.shape)-1, self.axis))
+
+        if self.k < 1 or self.k > input.shape[self.axis]:
+            raise ValueError("k must be in 1 ~ %d,now k is %d" %
+                             (input.shape[self.axis], self.k))
+
         out = torch.topk(input, k=self.k, dim=self.axis, sorted=True)[0]
         return out
         
