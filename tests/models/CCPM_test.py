9c84d66b6cc2bdb8b501ef096d5f16c6e474bca7
shenweichen <wcshen1994@163.com>
2019-09-24 21:30:36 +0800

    Update CCPM_test.py

--- tests/models/CCPM_test.py
+++ tests/models/CCPM_test.py
@@ -1,26 +1,43 @@
-# -*- coding: utf-8 -*-
 import pytest
-import sys
+
 from deepctr_torch.models import CCPM
-from tests.utils import get_test_data, SAMPLE_SIZE, check_model
+from tests.utils import check_model, get_test_data, SAMPLE_SIZE
 
 
 @pytest.mark.parametrize(
     'sparse_feature_num,dense_feature_num',
-    [(2,0),(3,0)
+    [ (3, 0)
      ]
 )
 def test_CCPM(sparse_feature_num, dense_feature_num):
     model_name = "CCPM"
+
     sample_size = SAMPLE_SIZE
     x, y, feature_columns = get_test_data(
         sample_size, sparse_feature_num, dense_feature_num)
 
-    model = CCPM(feature_columns, feature_columns,
-                dnn_hidden_units=[32, ], dnn_dropout=0.5, conv_kernel_width=(6, 5),
-                 conv_filters=(4, 4))
-    check_model(model, model_name, x, y,check_model_io=False)
+    model = CCPM(feature_columns,feature_columns, conv_kernel_width=(3, 2), conv_filters=(
+        2, 1), dnn_hidden_units=[32, ], dnn_dropout=0.5)
+    check_model(model, model_name, x, y)
+
+
+@pytest.mark.parametrize(
+    'sparse_feature_num,dense_feature_num',
+    [(2, 0),
+     ]
+)
+def test_CCPM_without_seq(sparse_feature_num, dense_feature_num):
+
+    model_name = "CCPM"
+
+    sample_size = SAMPLE_SIZE
+    x, y, feature_columns = get_test_data(
+        sample_size, sparse_feature_num, dense_feature_num, sequence_feature=())
+
+    model = CCPM(feature_columns, feature_columns,conv_kernel_width=(3, 2), conv_filters=(
+        2, 1), dnn_hidden_units=[32, ], dnn_dropout=0.5)
+    check_model(model, model_name, x, y)
 
 
 if __name__ == "__main__":
-   pass
+    pass
