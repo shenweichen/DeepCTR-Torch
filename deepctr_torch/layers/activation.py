1ffca58844eb4fa745909fc59176525af603364d
zyf <zhangyuef2014@163.com>
2019-12-17 18:22:27 -0800

    Add dim in Dice activation

--- deepctr_torch/layers/activation.py
+++ deepctr_torch/layers/activation.py
@@ -15,23 +15,43 @@ import torch.nn.functional as F
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 class Dice(nn.Module):
-    """
+    """The Data Adaptive Activation Function in DIN,which can be viewed as a generalization of PReLu and can adaptively adjust the rectified point according to distribution of input data.
+
+    Input shape:
+        - 2 dims: [batch_size, embedding_size(features)]
+        - 3 dims: [batch_size, num_features, embedding_size(features)]
+
+    Output shape:
+        - Same shape as the input.
     
     References
         - [Zhou G, Zhu X, Song C, et al. Deep interest network for click-through rate prediction[C]//Proceedings of the 24th ACM SIGKDD International Conference on Knowledge Discovery & Data Mining. ACM, 2018: 1059-1068.](https://arxiv.org/pdf/1706.06978.pdf)
         - https://github.com/zhougr1993/DeepInterestNetwork, https://github.com/fanoping/DIN-pytorch
     """
-    def __init__(self, num_features, epsilon=1e-9):
+    def __init__(self, num_features, dim=2, epsilon=1e-9):
         super(Dice, self).__init__()
+        assert dim == 2 or dim == 3
         self.bn = nn.BatchNorm1d(num_features, eps=epsilon)
         self.sigmoid = nn.Sigmoid()
+        self.dim = dim
 
-        self.alpha = torch.zeros((num_features,)).to(device)
-        
+        if self.dim == 2:
+            self.alpha = torch.zeros((num_features,)).to(device)
+        else:
+            self.alpha = torch.zeros((num_features, 1)).to(device)
+
     def forward(self, x):
-        assert x.dim() == 2
-        x_p = self.sigmoid(self.bn(x))
-        out = self.alpha * (1 - x_p) * x + x_p * x
+        # x shape: [batch_size, num_features, embedding_size(features)]
+        assert x.dim() == 2 or x.dim() == 3
+
+        if self.dim == 2:
+            x_p = self.sigmoid(self.bn(x))
+            out = self.alpha * (1 - x_p) * x + x_p * x
+        else:
+            x = torch.transpose(x, 1, 2)
+            x_p = self.sigmoid(self.bn(x))
+            out = self.alpha * (1 - x_p) * x + x_p * x
+            out = torch.transpose(out, 1, 2)
         
         return out
 
@@ -50,7 +70,7 @@ def activation_layer(activation, hidden_size=None, dice_dim=2):
         act_layer = nn.ReLU(inplace=True)
     elif activation.lower() == 'dice':
         assert dice_dim
-        act_layer = Dice(hidden_size)
+        act_layer = Dice(hidden_size, dice_dim)
     elif activation.lower() == 'prelu':
         act_layer = nn.PReLU()
     else:
--- deepctr_torch/layers/core.py
+++ deepctr_torch/layers/core.py
@@ -33,9 +33,8 @@ class DNN(nn.Module):
     """
 
     def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
-                 init_std=0.0001, seed=1024, device='cpu'):
+                 init_std=0.0001, dice_dim=3, seed=1024, device='cpu'):
         super(DNN, self).__init__()
-        self.activation = activation
         self.dropout_rate = dropout_rate
         self.dropout = nn.Dropout(dropout_rate)
         self.seed = seed
@@ -53,7 +52,7 @@ class DNN(nn.Module):
                 [nn.BatchNorm1d(hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
         
         self.activation_layers = nn.ModuleList(
-            [activation_layer(self.activation, hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
+            [activation_layer(activation, hidden_units[i + 1], dice_dim) for i in range(len(hidden_units) - 1)])
 
         for name, tensor in self.linears.named_parameters():
             if 'weight' in name:
