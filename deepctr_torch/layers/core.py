31795759b17aaf4f2de1c5f0ea9d47791408d7df
zyf <zhangyuef2014@163.com>
2019-12-21 18:31:30 -0800

    Add LocalActivationUnit.

--- deepctr_torch/layers/core.py
+++ deepctr_torch/layers/core.py
@@ -7,6 +7,65 @@ import torch.nn.functional as F
 from .activation import activation_layer
 
 
+class LocalActivationUnit(nn.Module):
+    """The LocalActivationUnit used in DIN with which the representation of
+        user interests varies adaptively given different candidate items.
+
+    Input shape
+        - A list of two 3D tensor with shape:  ``(batch_size, 1, embedding_size)`` and ``(batch_size, T, embedding_size)``
+
+    Output shape
+        - 3D tensor with shape: ``(batch_size, T, 1)``.
+
+    Arguments
+        - **hidden_units**:list of positive integer, the attention net layer number and units in each layer.
+
+        - **activation**: Activation function to use in attention net.
+
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix of attention net.
+
+        - **dropout_rate**: float in [0,1). Fraction of the units to dropout in attention net.
+
+        - **use_bn**: bool. Whether use BatchNormalization before activation or not in attention net.
+
+        - **seed**: A Python integer to use as random seed.
+
+    References
+        - [Zhou G, Zhu X, Song C, et al. Deep interest network for click-through rate prediction[C]//Proceedings of the 24th ACM SIGKDD International Conference on Knowledge Discovery & Data Mining. ACM, 2018: 1059-1068.](https://arxiv.org/pdf/1706.06978.pdf)
+    """
+
+    def __init__(self, hidden_units=[80, 40], embedding_dim=4, activation='Dice', dropout_rate=0, use_bn=False):
+        super(LocalActivationUnit, self).__init__()
+
+        self.dnn1 = DNN(inputs_dim=4*embedding_dim,
+                       hidden_units=hidden_units,
+                       activation=activation,
+                       dropout_rate=0.5,
+                       use_bn=use_bn,
+                       dice_dim=3)
+
+        # self.dnn2 = DNN(inputs_dim=hidden_units[-1],
+        #                  hidden_units=[1],
+        #                  activation=activation,
+        #                  use_bn=use_bn,
+        #                  dice_dim=3)
+
+        self.dense = nn.Linear(hidden_units[-1], 1)
+
+    def forward(self, query, user_behavior):
+        # query ad            : size -> batch_size * 1 * embedding_size
+        # user behavior       : size -> batch_size * time_seq_len * embedding_size
+
+        user_behavior_len = user_behavior.size(1)
+        queries = torch.cat([query for _ in range(user_behavior_len)], dim=1)
+
+        attention_input = torch.cat([queries, user_behavior, queries-user_behavior, queries*user_behavior], dim=-1)
+        attention_output = self.dnn1(attention_input)
+        attention_output = self.dense(attention_output)
+
+        return attention_output
+
+
 class DNN(nn.Module):
     """The Multi Layer Percetron
 
