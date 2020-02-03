e2ff3888b596fd5856f0750566e4a744f99e47e5
zyf <zhangyuef2014@163.com>
2020-02-03 12:44:30 -0800

    Add test for Dice activation layer

--- tests/layers/activation_test.py
+++ tests/layers/activation_test.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
 from deepctr_torch.layers import activation
+from tests.utils import layer_test
 
+
+def test_dice():
+    layer_test(activation.Dice, kwargs={'num_features': 3, 'dim': 2},
+               input_shape=(5, 3), expected_output_shape=(5,3))
