835dabd57cb6f3b02f9805141ae657ecc9543d8c
浅梦 <wcshen1994@163.com>
2020-10-17 23:13:17 +0800

    no message

--- tests/models/AFM_test.py
+++ tests/models/AFM_test.py
@@ -2,6 +2,7 @@
 import pytest
 
 from deepctr_torch.models import AFM
+from deepctr_torch.callbacks import EarlyStopping, ModelCheckpoint
 from ..utils import get_test_data, SAMPLE_SIZE, check_model, get_device
 
 
@@ -19,6 +20,12 @@ def test_AFM(use_attention, sparse_feature_num, dense_feature_num):
                 use_attention=use_attention, afm_dropout=0.5, device=get_device())
 
     check_model(model, model_name, x, y)
+
+
+    early_stopping = EarlyStopping(monitor='val_binary_crossentropy', min_delta=0, verbose=1, patience=0, mode='max', baseline=None)
+    model_checkpoint = ModelCheckpoint(filepath='model.ckpt', monitor='val_binary_crossentropy', verbose=1, save_best_only=True,
+                                       save_weights_only=False, mode='max', period=1)
+    model.fit(x, y, batch_size=64, epochs=3, validation_split=0.5,callbacks=[early_stopping, model_checkpoint])
 
 
 if __name__ == '__main__':
