61b5775bf695b695d205ac9154528480425fcebe
zanshuxun <zanshuxun@aliyun.com>
2020-10-23 15:54:57 +0800

    fix bug in #128

--- examples/run_din.py
+++ examples/run_din.py
@@ -14,9 +14,8 @@ def get_xy_fd():
                        SparseFeat('item', 3 + 1, embedding_dim=8), SparseFeat('item_gender', 2 + 1, embedding_dim=8),
                        DenseFeat('score', 1)]
 
-    feature_columns += [VarLenSparseFeat(SparseFeat('hist_item', 3 + 1, embedding_dim=8), 4),
-                        VarLenSparseFeat(SparseFeat('hist_item_gender', 2 + 1, embedding_dim=8), 4)]
-
+    feature_columns += [VarLenSparseFeat(SparseFeat('hist_item', 3 + 1, embedding_dim=8), 4, length_name="seq_length"),
+                        VarLenSparseFeat(SparseFeat('hist_item_gender', 2 + 1, embedding_dim=8), 4, length_name="seq_length")]
     behavior_feature_list = ["item", "item_gender"]
     uid = np.array([0, 1, 2])
     ugender = np.array([0, 1, 0])
@@ -26,9 +25,11 @@ def get_xy_fd():
 
     hist_iid = np.array([[1, 2, 3, 0], [1, 2, 3, 0], [1, 2, 0, 0]])
     hist_igender = np.array([[1, 1, 2, 0], [2, 1, 1, 0], [2, 1, 0, 0]])
+    behavior_length = np.array([3, 3, 2])
 
     feature_dict = {'user': uid, 'gender': ugender, 'item': iid, 'item_gender': igender,
-                    'hist_item': hist_iid, 'hist_item_gender': hist_igender, 'score': score}
+                    'hist_item': hist_iid, 'hist_item_gender': hist_igender, 'score': score,
+                    "seq_length": behavior_length}
     x = {name: feature_dict[name] for name in get_feature_names(feature_columns)}
     y = np.array([1, 0, 1])
 
@@ -43,7 +44,7 @@ if __name__ == "__main__":
         print('cuda ready...')
         device = 'cuda:0'
 
-    model = DIN(feature_columns, behavior_feature_list, device=device)
+    model = DIN(feature_columns, behavior_feature_list, device=device, att_weight_normalization=True)
     model.compile('adagrad', 'binary_crossentropy',
                   metrics=['binary_crossentropy'])
     history = model.fit(x, y, batch_size=3, epochs=10, verbose=2, validation_split=0.0)
